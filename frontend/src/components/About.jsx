467fbd819275a782c051f3c5332a4892cab0f868
Shubham04jha <shubhamjha70491@gmail.com>
2024-10-25 22:51:46 +0530

    commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+
+
+  return (
+      <div className="flex items-center justify-center pb-10">
+          <p className="text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
+          </p>
+      </div>
+  );
+};
+export default About;
